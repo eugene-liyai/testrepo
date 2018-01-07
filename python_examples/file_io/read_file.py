56ab0d7b1f69617b1e2d36ae2d86165cae0df2d0
Andela-eugene <eugene.liyai@andela.com>
2018-01-07 15:38:17 +0300

    [Fead] read file and display

--- python_examples/file_io/read_file.py
+++ python_examples/file_io/read_file.py
@@ -1,3 +1,8 @@
-## read file function
+# read file function
 def read_file():
-    file_data = open('input_file.txt')
+    file_data = open('./python_examples/file_io/input_file.txt')
+    for single_line in file_data:
+        print(single_line, end='')
+
+if __name__ == "__main__":
+    read_file()
