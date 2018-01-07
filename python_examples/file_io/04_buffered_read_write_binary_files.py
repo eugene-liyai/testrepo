cdccf02098e263bc9cc4cc2241dd445af88c1730
Andela-eugene <eugene.liyai@andela.com>
2018-01-07 16:30:18 +0300

    [Feat] implement binary read write

---
+++ python_examples/file_io/04_buffered_read_write_binary_files.py
@@ -0,0 +1,20 @@
+# read file function
+def read_file():
+    buffersize = 40000  # declare buffer size
+
+    # the 'rb' represents a read binary directive
+    file_data = open('./python_examples/file_io/sample.jpg', 'rb')
+
+    # the 'wb' represents a write binary directive
+    output_file = open('./python_examples/file_io/new.jpg', 'w')
+
+    buffer = file_data.read(buffersize)  # loop over the chunks of buffer
+    while len(buffer):
+        output_file.write(buffer)
+        buffer = file_data.read(buffersize)
+
+    print('Writing to file completed')
+
+
+if __name__ == "__main__":
+    read_file()
